98d36209d089c93a9c45e413cdcc203624f1e53b
Jokūbas Ramanauskas <jokubas.rama@gmail.com>
2015-10-04 14:08:05 +0300

    init

---
+++ JokubasRLithuanianNamesDeclensionBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace JokubasR\Bundle\LithuanianNamesDeclensionBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JokubasRLithuanianNamesDeclensionBundle extends Bundle
+{
+}
